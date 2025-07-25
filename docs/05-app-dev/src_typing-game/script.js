fa2dad6de7f2c893b7dc56f002227ef0401a117e
Yash Modi <modiyash2007@gmail.com>
2025-07-25 11:19:07 -0400

    Worked on script and fixed style slightly.

--- docs/05-app-dev/src_typing-game/script.js
+++ docs/05-app-dev/src_typing-game/script.js
@@ -2,16 +2,21 @@ import { base_sentences } from './sample_quotes.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const startButton = document.getElementById("start-button");
-  const quote = document.getElementById("quote");
+  const quoteElement = document.getElementById("quote");
   const typedValue = document.getElementById("typed-value");
   const startInfo = document.getElementById("start-info");
   const quotes = base_sentences;
+  let quote;
+  let words;
 
   startButton.addEventListener("click", displayQuote);
 
   function displayQuote() {
     startInfo.style.display = 'none';
-    quote.textContent = quotes[Math.floor(Math.random() * quotes.length)];
+    quote = quotes[Math.floor(Math.random() * quotes.length)];
+    words = quote.split(' ');
+    quoteElement.textContent = quote;
+    typedValue.value = '';
     typedValue.focus();
   }
 });
